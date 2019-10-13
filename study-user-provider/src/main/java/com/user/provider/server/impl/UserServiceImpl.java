48247d781f10095d965faf1ba7cbe4cf03b41c7a
翁志来_myvultr <3188894@qq.com>
2019-10-13 22:32:40 +0800

    添加Query

--- study-user-provider/src/main/java/com/user/provider/server/impl/UserServiceImpl.java
+++ study-user-provider/src/main/java/com/user/provider/server/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.user.provider.server.UserService;
 import com.wzl.commons.model.*;
 import com.wzl.commons.model.dto.DtoSave;
 import com.wzl.commons.model.entity.FaUserEntity;
+import com.wzl.commons.model.entity.view.FaUserRoleEntityView;
 import com.wzl.commons.model.mynum.DatabaseGeneratedOption;
 import com.wzl.commons.retention.EntityHelper;
 import org.apache.commons.lang3.StringUtils;
@@ -21,12 +22,18 @@ public class UserServiceImpl implements UserService {
     @Autowired
     MyBatisService<FaUserEntity> dapper;
 
+    @Autowired
+    MyBatisService<FaUserRoleEntityView> dapperUserRole;
+
     EntityHelper<FaUserEntity> eh = new EntityHelper<>(new FaUserEntity());
 
     @Override
     public ResultObj<FaUserEntity> singleByKey(DtoDo inEnt) {
         ResultObj<FaUserEntity> reObj=new ResultObj<>(true);
-        reObj.data=dapper.getSingleByPrimaryKey(eh, Convert.toInt(inEnt.key));
+        Integer key=Convert.toInt(inEnt.key);
+        reObj.data=dapper.getSingleByPrimaryKey(eh, key);
+        List<FaUserRoleEntityView> userRoleList=dapperUserRole.getAll(new EntityHelper<>(new FaUserRoleEntityView()),x->x.userId==key);
+        reObj.data.roleIdList=userRoleList.stream().map(x->x.roleId).collect(Collectors.toList());
         return reObj;
     }
 
@@ -58,8 +65,10 @@ public class UserServiceImpl implements UserService {
                 inEnt.whereList = Arrays.asList("id");
             }
             resultObj.data = dapper.update(eh, inEnt.saveFieldList, inEnt.whereList);
+            dapper.alter("delete from fa_user_role where USER_ID = "+eh.data.id);
         }
         resultObj.success = resultObj.data > 0;
+        dapper.alter(String.format("insert into fa_user_role(USER_ID,ROLE_ID) select %1$s USER_ID,ID ROLE_ID from fa_role where ID IN (%2$s) ", eh.data.id, StringUtils.join(inEnt.data.roleIdList,",")));
 
         return resultObj;
     }
